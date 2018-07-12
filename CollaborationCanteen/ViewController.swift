63c0ef43e3d01f708aec31defe571890b564fc96
zein rezky chandra <zeinrezky@gmail.com>
2018-07-12 14:44:29 +0700

    - Finishing my task

--- CollaborationCanteen/ViewController.swift
+++ CollaborationCanteen/ViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonNext: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Test
+        
+        setupUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +24,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setupUI(){
+        buttonNext.layer.cornerRadius = 5.0
+        buttonNext.layer.masksToBounds = true
+        let widthRandom = CGFloat(arc4random_uniform(320))
+        let heightRandom = CGFloat(arc4random_uniform(UInt32(self.view.frame.size.height)))
+        UIView.animate(withDuration: 0.5, animations: {
+            self.buttonNext.frame = CGRect(x: widthRandom, y: heightRandom, width: self.buttonNext.frame.size.width, height: self.buttonNext.frame.size.height)
+        }, completion: nil)
+    }
 }
 
